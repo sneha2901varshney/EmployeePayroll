d2b854670cbaa097cb1a5db10c36d0491f510e49
snehavarshney <snehavarshney111@gmail.com>
2025-03-08 14:14:06 +0530

    Added:Environment for Production and Development

--- employeePayroll/src/main/java/com/example/employeePayroll/Service/EmployeeService.java
+++ employeePayroll/src/main/java/com/example/employeePayroll/Service/EmployeeService.java
@@ -5,45 +5,65 @@ import com.example.employeePayroll.DTO.EmployeeDTO;
 import com.example.employeePayroll.Entities.EmployeeEntity;
 import com.example.employeePayroll.Interfaces.IEmployeeService;
 import com.example.employeePayroll.Repositories.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class EmployeeService implements IEmployeeService {
+
+
 
     @Autowired
     EmployeeRepository employeeRepository;
 
+    ObjectMapper obj = new ObjectMapper();
 
+    public EmployeeDTO get(Long id) throws Exception{
 
-    public EmployeeDTO get(Long id){
-
-        EmployeeEntity empFound = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Cannot find Employee with given id"));
+        EmployeeEntity empFound = employeeRepository.findById(id).orElseThrow(()->
+        {
+            log.error("Cannot find employee with id {}", id);
+            return new RuntimeException("Cannot find employee with given id");
+        });
 
         EmployeeDTO empDto = new EmployeeDTO(empFound.getName(), empFound.getSalary());
         empDto.setId(empFound.getId());
+
+        log.info("Employee DTO send for id: {} is : {}", id, obj.writeValueAsString(empDto));
 
         return empDto;
 
     }
 
-    public EmployeeDTO create(EmployeeDTO newEmp){
+    public EmployeeDTO create(EmployeeDTO newEmp) throws Exception{
 
         EmployeeEntity newEntity = new EmployeeEntity(newEmp.getName(), newEmp.getSalary());
 
         employeeRepository.save(newEntity);
 
+        log.info("Employee saved in db: {}", obj.writeValueAsString(newEntity));
+
         EmployeeDTO emp = new EmployeeDTO(newEntity.getName(), newEntity.getSalary());
 
         emp.setId(newEntity.getId());
 
+        log.info("Employee DTO sent: {}", obj.writeValueAsString(emp));
+
         return emp;
     }
 
-
-    public EmployeeDTO edit(EmployeeDTO emp, Long id){
+    public EmployeeDTO edit(EmployeeDTO emp, Long id)throws Exception{
         //finding employee
-        EmployeeEntity foundEmp = employeeRepository.findById(id).orElseThrow(()->new RuntimeException("No employee found for given id"));
+        EmployeeEntity foundEmp =  employeeRepository.findById(id).orElseThrow(()->
+        {
+            log.error("Cannot find employee with id : {}", id);
+            return new RuntimeException("cannot find employee with given id");
+        });
 
         //updating details
         foundEmp.setName(emp.getName());
@@ -51,6 +71,8 @@ public class EmployeeService implements IEmployeeService {
 
         //saving in database
         employeeRepository.save(foundEmp);
+
+        log.info("Employee saved after editing in db is : {}", obj.writeValueAsString(foundEmp));
 
         //creating dto to return
         EmployeeDTO employeeDTO = new EmployeeDTO(foundEmp.getName(), foundEmp.getSalary());
@@ -61,10 +83,13 @@ public class EmployeeService implements IEmployeeService {
 
     }
 
-
     public String delete(Long id){
 
-        EmployeeEntity foundEmp = employeeRepository.findById(id).orElseThrow(()->new RuntimeException("No employee found for given id"));
+        EmployeeEntity foundEmp = employeeRepository.findById(id).orElseThrow(()->
+        {
+            log.error("Cannot find user with id : {}", id);
+            return new RuntimeException("cannot find user with given id");
+        });
 
         employeeRepository.delete(foundEmp);
 
